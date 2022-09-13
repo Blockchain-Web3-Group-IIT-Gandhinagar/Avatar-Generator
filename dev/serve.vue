f8ffb27f2f5f3da309f4497724b18978a041ab2f
Mumuksh29 <mumukshtayal29@gmail.com>
2022-09-13 22:48:20 +0530

    added bootstrap card (basic)

--- dev/serve.vue
+++ dev/serve.vue
@@ -33,21 +33,35 @@ export default defineComponent({
     /> -->
     
     <br/><br/><br/>
-    <zombie-char-component
+    <!-- <zombie-char-component
       :isZombieLoaded="true"
       :optionalDna=null
       :zombieName="name"
       :autoGenerate="true"
-    />
+    /> -->
     <br/><br/><br/>
     <ul>
     <li v-for="index in 10" :key="index">
-      <zombie-char-component
+      <div class="card" style="width: 18rem;">
+        
+        <zombie-char-component class="card-img-top"
       :isZombieLoaded="true"
       :optionalDna=null
       :zombieName="name_list[index]"
       :autoGenerate="true"
     />
+        <div class="card-body">
+          <h5 class="card-title">ZombieName</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      
+      <!-- <zombie-char-component
+      :isZombieLoaded="true"
+      :optionalDna=null
+      :zombieName="name_list[index]"
+      :autoGenerate="true"
+    /> -->
     </li>
     </ul>
 
